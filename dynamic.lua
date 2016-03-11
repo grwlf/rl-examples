7edd8e20fe7719dd2add42368599fbdcda483486
Dmitry Vyal <akamaus@gmail.com>
2016-03-11 15:58:42 +0300

    naive policy improvement

--- dynamic.lua
+++ dynamic.lua
@@ -29,6 +29,29 @@ function evaluate_states(trans, policy, reward)
    return V
 end
 
+function improve_policy(V)
+   local P = torch.zeros(NA,NA)
+
+   for x=1,4 do
+      for y=1,4 do
+         local max
+         local max_i = 0
+         for a=1,NA do
+            local x2,y2 = trans(x,y, a)
+            local R = reward(x,y, x2,y2, a)
+            local v = R + gamma * V[x2][y2]
+
+            if max_i == 0 or max < v then
+               max_i = a
+               max = v
+            end
+         end
+         P[x][y] = max_i
+      end
+   end
+   return P
+end
+
 function is_terminal(x,y)
    return x == 1 and y == 1 or x == N and y == N
 end
@@ -51,10 +74,6 @@ function trans(x,y,a)
 end
 end
 
-function policy(x,y,a)
-   return 0.25
-end
-
 function reward(x,y, x2,y2, a)
    if is_terminal(x2,y2) then return 0
    elseif x == x2 and y == y2 then return -3
@@ -62,5 +81,26 @@ function reward(x,y, x2,y2, a)
    end
 end
 
-vs = evaluate_states(trans, policy, reward)
-print(vs)
+function recursive_policy_improvement()
+   local policy = function (x,y,a) return 0.25 end
+   local old_P = torch.zeros(NA,NA)
+   local vs
+   local stale
+   repeat
+      vs = evaluate_states(trans, policy, reward)
+      print(vs)
+      local P = improve_policy(vs)
+      stale = torch.all(torch.eq(old_P,P))
+      old_P = P
+      policy = function(x,y,a)
+         if P[x][y] == a then return 1
+         else return 0
+         end
+      end
+      print(P)
+   until stale
+
+   return vs, P
+end
+
+recursive_policy_improvement()
