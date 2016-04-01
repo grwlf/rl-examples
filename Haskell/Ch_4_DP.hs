d2b011a708b1e2291aa7cf6c44b15ab5a1d10775
Sergey Mironov <grrwlf@gmail.com>
2016-04-01 09:40:48 +0000

    Implement policy evaluation

--- Haskell/Ch_4_DP.hs
+++ Haskell/Ch_4_DP.hs
@@ -9,14 +9,20 @@
 {-# LANGUAGE FunctionalDependencies #-}
 module Ch_4_DP where
 
+import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans
+import Control.Monad.State.Strict
+import Control.Break
 import Data.Ratio
-import Data.List as List
+import Data.List hiding (break)
+import qualified Data.List as List
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Prelude hiding(break)
+import Text.Printf
 
 type Point = (Int,Int)
 
@@ -36,7 +42,12 @@ data StateVal s = StateVal {
 class (Ord s) => RLProblem p s a | p -> s , p -> a where
   rl_states :: p -> Set s
   rl_actions :: p -> s -> Set a
-  rl_transitions :: p -> s -> a -> Set (Probability, Reward, s)
+  rl_transitions :: p -> s -> a -> Set (Probability, (Reward, s))
+
+
+rl_prob_invariant :: forall p s a . (RLProblem p s a) => p -> s -> a -> Bool
+rl_prob_invariant p s a = 1%1 == List.sum (map fst (Set.toList $ rl_transitions p s a))
+
 
 data Policy s a = Policy {
     pol_map :: Map s a
@@ -48,24 +59,31 @@ data EvalOpts = EvalOpts {
 
 policy_eval :: forall p s a m . (RLProblem p s a, MonadIO m)
   => p -> Policy s a -> EvalOpts -> StateVal s -> m (StateVal s)
-
 policy_eval p Policy{..} EvalOpts{..} StateVal{..} = do
   let v = v_map
-  let sum l f = do
-        x <- forM (Set.toList l) f
-        return (List.sum x)
+  let sum l f = List.sum <$> forM (Set.toList l) f
+  let get_delta = gets fst
+  let put_delta d = modify (\(_,v) -> (d, v))
+  let get_v s = (! s) <$> gets snd
+  let put_v s v_s = modify (\(d,v) -> (d, Map.insert s v_s v))
 
-  v' <- forM (Set.toList $ rl_states p) $ \s -> do
-    temp <- pure $ v ! s
-    v_s <- sum (rl_actions p s) $ \a -> do
-              sum (rl_transitions p s a) $ \(p, r, s') -> do
-                return $ (fromRational p) * (r + eo_gamma * (v ! s'))
-    return (v_s, temp - v_s)
+  StateVal . snd <$> do
+    flip execStateT (0,v) $ loop $ do
 
-  return (StateVal v')
+      d <- get_delta
+      when (d < eo_gamma) $ do
+        break ()
 
-  undefined
+      forM_ (rl_states p) $ \s -> do
+        v_s <- get_v s
+        v_s' <- do
+          sum (rl_actions p s) $ \a -> do
 
+            sum (rl_transitions p s a) $ \(p, (r, s')) -> do
+              pure $ (fromRational p) * (r + eo_gamma * (v ! s'))
 
+        put_v s v_s'
+        d <- get_delta
+        put_delta (d`max`(abs (v_s - v_s')))
 
 
