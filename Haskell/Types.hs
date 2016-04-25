800d5b9df4bfc1e91f351d242fec41d516b07171
Sergey Mironov <grrwlf@gmail.com>
2016-04-25 12:25:49 +0300

    Show histogram

--- Haskell/Types.hs
+++ Haskell/Types.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE FunctionalDependencies #-}
@@ -6,6 +7,8 @@ module Types where
 
 import Control.Monad
 import Control.Monad.Trans
+import Control.Monad.Random
+import Control.Monad.State.Strict
 import Data.Ratio
 import Data.List hiding (break)
 import qualified Data.List as List
@@ -13,6 +16,7 @@ import Data.Map.Strict (Map, (!))
 import qualified Data.Map.Strict as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
+import System.Random
 
 debug :: (MonadIO m) => String -> m ()
 debug = liftIO . putStrLn
@@ -27,4 +31,26 @@ data StateVal s = StateVal {
 data GenericPolicy s a = GenericPolicy {
     gp_actions :: Map s (Set (Probability,a))
   } deriving(Eq,Ord, Show)
+
+
+
+
+data Histogram a = Histogram {
+    hist_map :: Map a Integer
+  } deriving(Show)
+
+showHist :: (Ord a) => Histogram a -> IO ()
+showHist Histogram{..} = do
+  let max = fromInteger $ maximum $ map snd $ Map.toAscList hist_map
+  forM_ (Map.toAscList hist_map`zip`[0..]) $ \((k,fromInteger -> v),i) -> do
+    putStrLn $ show i ++ " " ++ (replicate ((v * 50)`div`max) '#')
+
+sample :: (Ord a) => Int -> (StdGen -> (a,StdGen)) -> Histogram a
+sample n f =
+  (Histogram . fst) $
+  flip runRand (mkStdGen 33) $ do
+  flip execStateT Map.empty $ do
+    replicateM_ n $ do
+      x <- lift $ liftRand f
+      modify $ Map.insertWith (+) x 1
 
