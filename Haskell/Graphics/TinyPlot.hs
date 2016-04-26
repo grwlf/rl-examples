20d13abdeb9c1fbb1f0c8f32743868a9dd13c013
Sergey Mironov <grrwlf@gmail.com>
2016-04-26 14:09:18 +0000

    TinyPlot, add sketch

--- Haskell/Graphics/TinyPlot.hs
+++ Haskell/Graphics/TinyPlot.hs
@@ -1,1 +1,19 @@
 module TinyPlot where
+
+data PlotStream = PlotStream {
+    ps_filename :: String
+  , ps_handle :: Handle
+  } deriving(Show)
+
+push :: PlotStream -> Double -> Double -> IO ()
+push PlotStream{..} x y = hPutStrLn ps_handle $ show x ++ " " ++ show y
+
+data Plot {
+    pl_program :: String
+  } deriving(Show)
+
+
+spawnPlot :: Plot -> IO ()
+spawnPlot = System.runProcess
+
+
